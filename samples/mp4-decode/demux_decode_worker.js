51411619df201333178a543027650a08a967c5b6
sklein12 <sklein12@gmail.com>
2022-05-21 16:29:36 +0000

    Deploying to gh-pages from @ sklein12/w3c-webcodecs@b587b270ad4881a180250ec430198052f2e5d96a 🚀

--- samples/mp4-decode/demux_decode_worker.js
+++ samples/mp4-decode/demux_decode_worker.js
@@ -1,42 +1,69 @@
-importScripts('./mp4box.all.min.js');
-importScripts('./mp4_demuxer.js');
+importScripts("./mp4box.all.min.js");
+importScripts("./mp4_demuxer.js");
+importScripts("./encode-worker.js");
 
-self.addEventListener('message', function(e) {
+const encodeWorker = new Worker("./encode-worker.js");
+let stopDecoding = false;
+self.addEventListener("message", function (e) {
+  if (e.data.stop) {
+    endoceWorker.postMessage({
+      type: "stop",
+    });
+  }
   let offscreen = e.data.canvas;
-  let ctx = offscreen.getContext('2d');
+  let fileHandle = e.data.fileHandle;
+  let ctx = offscreen.getContext("2d");
   let startTime = 0;
   let frameCount = 0;
 
   let demuxer = new MP4Demuxer("/w3c-webcodecs/samples/media/bbb.mp4");
 
   function getFrameStats() {
-      let now = performance.now();
-      let fps = "";
+    let now = performance.now();
+    let fps = "";
 
-      if (frameCount++) {
-        let elapsed = now - startTime;
-        fps = " (" + (1000.0 * frameCount / (elapsed)).toFixed(0) + " fps)"
-      } else {
-        // This is the first frame.
-        startTime = now;
-      }
+    if (frameCount++) {
+      let elapsed = now - startTime;
+      console.log(
+        " (" +
+          ((1000.0 * frameCount) / elapsed).toFixed(0) +
+          " fps) - " +
+          Math.round(elapsed / 1000)
+      );
+    } else {
+      // This is the first frame.
+      startTime = now;
+    }
 
-      return "Extracted " + frameCount + " frames" + fps;
+    return "Extracted " + frameCount + " frames" + fps;
   }
+  const encoder = encodeWorker.postMessage({
+    type: "init",
+    fileHandle,
+    trackSettings: {
+      width: 1280,
+      height: 720,
+    },
+  });
 
   let decoder = new VideoDecoder({
-    output : frame => {
+    output: (frame, arg2) => {
+      debugger;
       ctx.drawImage(frame, 0, 0, offscreen.width, offscreen.height);
+      encodeWorker.postMessage({
+        type: "encode",
+        frame,
+      });
 
       // Close ASAP.
       frame.close();
 
       // Draw some optional stats.
-      ctx.font = '35px sans-serif';
+      ctx.font = "35px sans-serif";
       ctx.fillStyle = "#ffffff";
       ctx.fillText(getFrameStats(), 40, 40, offscreen.width);
     },
-    error : e => console.error(e),
+    error: (e) => console.error(e),
   });
 
   demuxer.getConfig().then((config) => {
@@ -44,6 +71,8 @@ self.addEventListener('message', function(e) {
     offscreen.width = config.codedWidth;
 
     decoder.configure(config);
-    demuxer.start((chunk) => { decoder.decode(chunk); })
+    demuxer.start((chunk) => {
+      decoder.decode(chunk);
+    });
   });
-})
+});
